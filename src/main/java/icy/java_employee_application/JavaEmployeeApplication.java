5d46d17902ff5b3e0312fcc3f757414547383ced
icy-s <evgeny.tailov@gmail.com>
2026-01-10 15:39:51 +0200

    init commit

---
+++ src/main/java/icy/java_employee_application/JavaEmployeeApplication.java
@@ -0,0 +1,13 @@
+package icy.java_employee_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icy/java_employee_application/JavaEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package icy.java_employee_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
