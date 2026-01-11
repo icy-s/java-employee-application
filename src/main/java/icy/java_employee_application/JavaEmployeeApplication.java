27a2bebc81d7636d7f9497993f4320447cf5df76
icy-s <evgeny.tailov@gmail.com>
2026-01-11 10:53:29 +0200

    simple api

--- src/main/java/icy/java_employee_application/JavaEmployeeApplication.java
+++ src/main/java/icy/java_employee_application/JavaEmployeeApplication.java
@@ -2,12 +2,26 @@ package icy.java_employee_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+// аннотация ниже позволяет этому классу быть обработчиком HTTP запросов (REST API)
+@RestController
 @SpringBootApplication
 public class JavaEmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEmployeeApplication.class, args);
+	}
+
+	// метод ниже является обработчиком для GET запросов
+	@GetMapping
+	public List<String> helloWorld() {
+		return List.of(
+			"Hello World!",
+			"Hello World!");
 	}
 
 }
